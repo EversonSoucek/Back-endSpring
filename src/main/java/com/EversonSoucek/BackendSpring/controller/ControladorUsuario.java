80782dca84ad6cc8c57ce256fd6d63071132e448
Everson <sousekjuniorr@outlook.com>
2023-11-15 20:45:15 -0300

    Feito getMapping

--- src/main/java/com/EversonSoucek/BackendSpring/controller/ControladorUsuario.java
+++ src/main/java/com/EversonSoucek/BackendSpring/controller/ControladorUsuario.java
@@ -3,9 +3,12 @@ package com.EversonSoucek.BackendSpring.controller;
 import com.EversonSoucek.BackendSpring.model.Usuario;
 import com.EversonSoucek.BackendSpring.repository.RepositorioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ControladorUsuario {
@@ -18,4 +21,8 @@ public class ControladorUsuario {
         return repositorioUsuario.save(novoUsuario);
     }
 
+    @GetMapping("/usuarios")
+    List<Usuario> getTodosUsuarios(){
+        return repositorioUsuario.findAll();
+    }
 }
